e2b6f9763691c5d6a20c0288d39d3a14ee8ff893
Chandler Supple <36590015+chandlersupple@users.noreply.github.com>
2018-05-17 21:45:29 -0700

    Create standard_deviation.py

---
+++ standard_deviation.py
@@ -0,0 +1,17 @@
+import math
+
+def standard_deviation(input_x):
+    x = input_x.split(',')
+    x = list(map(int, x))
+    n = (len(x))
+    x_bar = 0
+    numerator = 0
+    for num in range (0, n):
+        x_bar = x_bar + x[num]
+    x_bar = x_bar / (n)
+    for num in range (0, n):
+        numerator = numerator + ((x[num] - x_bar)**2)
+    sd_under_root = numerator / (n)
+    sd = math.sqrt(sd_under_root)
+    print('Dataset Given: %s' %(x))
+    print('Standard Deviation: %s' %(sd))
